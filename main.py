614551b402714bfa351dc592a12ad81f00b48918
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 15:22:45 +0900

    Python while else statement example

--- main.py
+++ main.py
@@ -7,3 +7,44 @@ if __name__ == "__main__":
   else:
     # else clause code block
   """
+
+  # Python while else statement example
+
+  basket = [
+    {"fruit": "apple", "qty": 20},
+    {"fruit": "banana", "qty": 30},
+    {"fruit": "orange", "qty": 10}
+  ]
+
+  fruit = input("Enter a fruit: ")
+
+  index = 0
+  found = False
+
+  # while index < len(basket):
+  #   item = basket[index]
+
+  #   if item["fruit"] == fruit:
+  #     found = True
+  #     print(f"The basket has {item['qty']} {item['fruit']}(s)")
+  #     break
+
+  #   index += 1
+
+  # if not found:
+  #   qty = int(input(f"Enter the qty for {fruit}: "))
+  #   basket.append({"fruit": fruit, "qty": qty})
+  #   print(basket)
+
+  while index < len(basket):
+    item = basket[index]
+
+    if item["fruit"] == fruit:
+      print(f"The basket has {item['qty']} {item['fruit']}(s)")
+      break
+
+    index += 1
+  else:
+    qty = int(input(f"Enter the qty for {fruit}: "))
+    basket.append({"fruit": fruit, "qty": qty})
+    print(basket)
