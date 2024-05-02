1807a1b2d58b071c9ea264f5b35da5fb407a4ea8
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2024-05-02 15:18:37 +0900

    Introduction to Python while else statement

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 if __name__ == "__main__":
-  print("Python while else")
+  # Introduction to Python while else statement
+
+  """
+  while condition:
+    # code block to run
+  else:
+    # else clause code block
+  """
